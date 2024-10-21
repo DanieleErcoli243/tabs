1b22b31b9472556f693fb97b44dfd985dc77a34e
Polvere243 <danielercoli@gmail.com>
2024-10-21 10:33:24 +0200

    added an event listener to the parent container

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // seleziono gli elementi dal DOM
 const about = document.querySelector('.about');
 const btns = document.querySelectorAll('.tab-btn');
-const articles = document.querySelectorAll('.content'); 
+const articles = document.querySelectorAll('.content');
+
+// aggancio un ascoltatore di eventi al contenitore genitore
+
+about.addEventListener('click', e => {
+
+})
